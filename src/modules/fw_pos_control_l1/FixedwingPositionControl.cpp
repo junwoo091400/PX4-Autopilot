a0674f469440e9a27a8a39a854374f99034d577c
Jaeyoung Lim <jalim@ethz.ch>
2022-01-19 14:39:59 +0100

    Use current_sp for publishing orbit status

--- src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
+++ src/modules/fw_pos_control_l1/FixedwingPositionControl.cpp
@@ -873,11 +873,13 @@ FixedwingPositionControl::control_auto(const hrt_abstime &now, const Vector2d &c
 		break;
 
 	case position_setpoint_s::SETPOINT_TYPE_VELOCITY:
-		control_auto_velocity(now, dt, curr_pos, ground_speed, pos_sp_prev, pos_sp_curr);
+		control_auto_velocity(now, dt, curr_pos, ground_speed, pos_sp_prev, current_sp);
 		break;
 
 	case position_setpoint_s::SETPOINT_TYPE_LOITER:
 		control_auto_loiter(now, dt, curr_pos, ground_speed, pos_sp_prev, current_sp, pos_sp_next);
+		publishOrbitStatus(current_sp);
+
 		break;
 
 	case position_setpoint_s::SETPOINT_TYPE_LAND:
