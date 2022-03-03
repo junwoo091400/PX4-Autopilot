0d813be32841e5785dcaf661e0540262a2a3d39f
Junwoo Hwang <junwoo@auterion.com>
2022-03-03 20:07:10 +0100

    Follow Target : Bug fixes and first working version of rate based control logic, still buggy

--- src/modules/flight_mode_manager/tasks/AutoFollowTarget/FlightTaskAutoFollowTarget.hpp
+++ src/modules/flight_mode_manager/tasks/AutoFollowTarget/FlightTaskAutoFollowTarget.hpp
@@ -135,6 +135,13 @@ protected:
 		FOLLOW_GIMBAL_MODE_3D
 	};
 
+	void update_target_pose_filter(follow_target_estimator_s follow_target_estimator);
+	float update_target_orientation(Vector2f target_velocity);
+	float update_orbit_angle(float target_orientation, float fllow_angle, float max_orbital_rate);
+	Vector3f calculate_drone_desired_position(Vector3f target_position);
+
+	void point_gimbal_at(float xy_distance, float z_distance);
+
 	/**
 	 * Get the current follow-me perspective setting from PX4 parameters
 	 *
@@ -142,8 +149,6 @@ protected:
 	 * @return Angle [deg] from which the drone should view the target while following it, with zero degrees indicating the target's 12 o'clock
 	 */
 	float update_follow_me_angle_setting(int param_nav_ft_fs) const;
-
-	void point_gimbal_at(float xy_distance, float z_distance);
 
 	// Estimator for target position and velocity
 	TargetEstimator _target_estimator;
