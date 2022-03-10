851dcd24d4eb103c12a3ed85b973ea76753923ef
Junwoo Hwang <junwoo@auterion.com>
2022-03-10 17:01:17 +0100

    Follow Target : Implementing RC control user input for Follow Target control
    Follow Target : edit to conform to updated unwrap_pi function
    
    Follow Target : Make follow angle, distance and height RC command input configurable
    
    Follow Target : Make Follow Target not interruptable by moving joystick in Commander
    
    Follow Target : reconfigure yaw, pitch and roll for better user experience in RC adjusting configurations, and add angular rate limit taking target distance into account
    
    Follow Target : Change RC channels used for adjustments and re-order header file for clarity
    
    Follow Target : Fix Parameters custom parent macro, since using DEFINE_PARAMETERS alone misses the parameter updates of the parent class, FlightTask
    
    Follow Target : Fix Orbit Tangential speed actually being angular rate bug, which was causing a phenomenon of drnoe getting 'dragged' towards the target velocity direction
    
    Follow Target : Final tidying and refactoring for master merge - step 1
    
    Add more comments on header file
    
    Follow Target : tidy, remove unnecessary debug uORB elements from follow_target_status message
    
    Follow Target : Turn off Yaw filtering by default
    
    Follow Target : Tidy maximum orbital velocity calculation
    
    Follow Target : add yaw setpoint filter time constant parameter for testing and fix NAV_FT_HT title
    
    Follow Target : Add RC adjustment window logic to prevent drone not catching up the change of follow target parameters
    
    Follow Target : fixes
    
    Follow Target: PR tidy few edits remove, and update comments

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -3350,7 +3350,6 @@ Commander::update_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_RTL:
-	case vehicle_status_s::NAVIGATION_STATE_AUTO_FOLLOW_TARGET:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_LAND:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_LANDENGFAIL:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_PRECLAND:
@@ -3419,6 +3418,9 @@ Commander::update_control_mode()
 
 		break;
 
+	// Follow Target supports RC adjustment, so disable auto control mode, which forces
+	// the exit of the Flight Task when RC adjustment (Stick movement) is detected.
+	case vehicle_status_s::NAVIGATION_STATE_AUTO_FOLLOW_TARGET:
 	case vehicle_status_s::NAVIGATION_STATE_ORBIT:
 		_vehicle_control_mode.flag_control_manual_enabled = false;
 		_vehicle_control_mode.flag_control_auto_enabled = false;
