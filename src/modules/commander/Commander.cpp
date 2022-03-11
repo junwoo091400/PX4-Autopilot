077ffc8d209d261a1ff08718675041bfb6cfd7a3
Junwoo Hwang <junwoo@auterion.com>
2022-03-11 11:39:04 +0100

    Follow Target : Make Follow Target not interruptable by moving joystick in Commander

--- src/modules/commander/Commander.cpp
+++ src/modules/commander/Commander.cpp
@@ -3351,7 +3351,6 @@ Commander::update_control_mode()
 		break;
 
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_RTL:
-	case vehicle_status_s::NAVIGATION_STATE_AUTO_FOLLOW_TARGET:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_LAND:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_LANDENGFAIL:
 	case vehicle_status_s::NAVIGATION_STATE_AUTO_PRECLAND:
@@ -3420,6 +3419,7 @@ Commander::update_control_mode()
 
 		break;
 
+	case vehicle_status_s::NAVIGATION_STATE_AUTO_FOLLOW_TARGET:
 	case vehicle_status_s::NAVIGATION_STATE_ORBIT:
 		_vehicle_control_mode.flag_control_manual_enabled = false;
 		_vehicle_control_mode.flag_control_auto_enabled = false;
